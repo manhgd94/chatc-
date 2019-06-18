16202d3c49822f2f2e536d6ac8c92b46534f8e3f
ManhGD <manhgd@fsoft.com.vn>
2019-06-18 17:30:32 +0900

    init

---
+++ helloworld/server/Server.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace helloworld
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            Connect();
+        }
+
+        /// <summary>
+        /// dong ket noi
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Server_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Close();
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            foreach (Socket item in clientList)
+            {
+                Send(item);
+            }
+            AddMessage(txbMessage.Text);
+        }
+
+        IPEndPoint IP;
+        Socket server;
+        List<Socket> clientList;
+
+        /// <summary>
+        /// connect to server
+        /// </summary>
+        void Connect()
+        {
+            clientList = new List<Socket>();
+            IP = new IPEndPoint(IPAddress.Any, 9999);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+            server.Bind(IP);
+
+            Thread Listen = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        server.Listen(100);
+                        Socket client = server.Accept();
+                        clientList.Add(client);
+
+                        Thread receive = new Thread(Receive);
+                        receive.IsBackground = true;
+                        receive.Start(client);
+                    }
+                }
+                catch
+                {
+                    IP = new IPEndPoint(IPAddress.Any, 9999);
+                    server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                }
+            });
+            Listen.IsBackground = true;
+            Listen.Start();
+        }
+
+        /// <summary>
+        /// close connect
+        /// </summary>
+        void Close()
+        {
+            server.Close();
+        }
+
+        /// <summary>
+        /// send message
+        /// </summary>
+        void Send(Socket client)
+        {
+            if (txbMessage.Text != string.Empty)
+                client.Send(Serialize(txbMessage.Text));
+        }
+
+        /// <summary>
+        /// receive message
+        /// </summary>
+        void Receive(object obj)
+        {
+            Socket client = obj as Socket;
+            try
+            {
+                while (true)
+                {
+                    byte[] data = new byte[1024 * 5000];
+                    client.Receive(data);
+
+                    string message = (string)Deserialize(data);
+
+                    AddMessage(message);
+                }
+            }
+            catch
+            {
+
+                clientList.Remove(client);
+                client.Close();
+            }
+        }
+
+        /// <summary>
+        /// add message vao khung chat
+        /// </summary>
+        /// <param name="s"></param>
+        void AddMessage(string s)
+        {
+            lsvMessage.Items.Add(new ListViewItem() { Text = s });
+        }
+
+        /// <summary>
+        /// phan manh
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        byte[] Serialize(object obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(stream, obj);
+
+            return stream.ToArray();
+        }
+
+        /// <summary>
+        /// gom manh
+        /// </summary>
+        /// <returns></returns>
+        object Deserialize(byte[] data)
+        {
+            MemoryStream stream = new MemoryStream(data);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            return formatter.Deserialize(stream);
+        }
+    }
+}
